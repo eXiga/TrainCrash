0e8423d9ebfe10dd88cbc139d6d942f502fc6343
Anton Kostenich <anton.kostenich@hotmail.com>
2014-11-17 14:32:23 +0300

    Encapsulate skybox property

--- TrainCrash/main.cpp
+++ TrainCrash/main.cpp
@@ -3,8 +3,10 @@
 int main (int argc, char** argv) {
 	GLContext * context = new GLContext (&argc, argv);
 	Camera * camera = new Camera ();
+	Skybox * skybox = new Skybox ();
 	context->SetContext(context);
 	context->SetCamera(camera);
+	context->SetSkybox(skybox);
 	context->Start();
 	return 0;
 }
